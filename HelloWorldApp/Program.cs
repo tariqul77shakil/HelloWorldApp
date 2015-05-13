9a00986e1150f4e7e7e73802659f76fe50d73026
tariqul77shakil <shakil77cse@gmail.com>
2015-05-13 10:54:03 +0600

    I added HellowWorldAPP

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age;
+            Console.WriteLine("Enter your name.\n\n\n");
+            string nameTitle = Console.ReadLine();
+            Console.WriteLine("Enter your age.\n\n\n");
+            age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("\n\nYour name is = " + nameTitle);
+            Console.WriteLine("Your Age is = " + age);
+            Console.ReadKey();
+        }
+    }
+}
